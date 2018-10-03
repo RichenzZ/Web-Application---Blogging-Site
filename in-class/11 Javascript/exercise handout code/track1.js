a25e4f07fa76dce0b863547f3143b94a72d07a30
RichenzZ <zhaoc996@gmail.com>
2018-10-03 18:01:53 -0400

    in class

---
+++ in-class/11 Javascript/exercise handout code/track1.js
@@ -0,0 +1,9 @@
+
+function addText() {
+    var input = document.getElementById('textfield');
+    var output = document.getElementById('todolist');
+    var li = "<li>" + input.value + "</li>";
+    node = document.createElement('li')
+    // output = output + li;
+    document.getElementById("todolist").appendChild();
+}
