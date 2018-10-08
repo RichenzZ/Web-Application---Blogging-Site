e0d9a49da6707b9f91a5a57267f4f6d723061170
RichenzZ <zhaoc996@gmail.com>
2018-10-08 16:08:27 -0400

    javacript

--- in-class/12 Ajax/fortune.js
+++ in-class/12 Ajax/fortune.js
@@ -1,5 +1,28 @@
+var req;
 
-function getFortune() {
+function sendRequest(){
+	if(window.XMLHttpRequest){
+		req = new XMLHttpRequest();
+	}
+	else{
+		req = new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	req.onreadystatechange = getFortune;
+	req.open("GET", "https://garrod.isri.cmu.edu/webapps/fortune/", true);
+	req.send();
 
 }
+function getFortune() {
+	console.log(req);
+	if(req.readyState != 4 || req.status != 200){
+		return;
+	}
+	var content = document.getElementById("content");
+	var items = JSON.parse(req.responseText);
+	var text = items["fortune"];
+	var newitem = document.createElement("p");
+	newitem.innerHTML = text;
+	content.append(newitem);
+}
 
+// window.setInterval(sendRequest, 10000);
